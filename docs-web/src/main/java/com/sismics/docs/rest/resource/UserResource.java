9f52633eddfdf9cac98b6750cff78403baa75085
LXBarry <liuxuyang151@163.com>
2026-03-16 14:44:19 +0800

    Add visiting as guest option

--- docs-web/src/main/java/com/sismics/docs/rest/resource/UserResource.java
+++ docs-web/src/main/java/com/sismics/docs/rest/resource/UserResource.java
@@ -295,10 +295,8 @@ public class UserResource extends BaseResource {
         UserDao userDao = new UserDao();
         User user = null;
         if (Constants.GUEST_USER_ID.equals(username)) {
-            if (ConfigUtil.getConfigBooleanValue(ConfigType.GUEST_LOGIN)) {
-                // Login as guest
-                user = userDao.getActiveByUsername(Constants.GUEST_USER_ID);
-            }
+            // Login as guest
+            user = userDao.getActiveByUsername(Constants.GUEST_USER_ID);
         } else {
             // Login as a normal user
             user = AuthenticationUtil.authenticate(username, password);
